21110014a19b767308dc8738a18c869b36ea319e
Ovidiu <enescu_vd@yahoo.com>
2017-12-03 16:50:33 +0200

    .

---
+++ app/src/main/java/com/londonappbrewery/quizzler/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.londonappbrewery.quizzler;
+
+/**
+ * Created by enesc on 03-Dec-17.
+ */
+
+public class TrueFalse {
+    private int mQuestionID;
+    private boolean mAnswer;
+
+    public TrueFalse(int questionResourceID, boolean trueOrFalse) {
+        mQuestionID = questionResourceID;
+        mAnswer = trueOrFalse;
+    }
+
+    public int getmQuestionID() {
+        return mQuestionID;
+    }
+
+    public void setmQuestionID(int mQuestionID) {
+        this.mQuestionID = mQuestionID;
+    }
+
+    public boolean ismAnswer() {
+        return mAnswer;
+    }
+
+    public void setmAnswer(boolean mAnswer) {
+        this.mAnswer = mAnswer;
+    }
+}
